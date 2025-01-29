bc9f80677555a1b9256b3ada5bc9dc48359c2718
Luisfer <1110241@galileo.edu>
2025-01-28 21:31:30 -0600

    elimino rutas sin uso

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PrintFGLController;
-use App\Http\Controllers\WebClientController;
 use App\Http\Controllers\PrintVanillaController;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('home', [HomeController::class, 'index']);
-Route::get('home/index', [HomeController::class, 'index']);
-Route::get('home/printFGL', [HomeController::class, 'printFGL']);
-Route::get('PrintFGLController', [PrintFGLController::class, 'printCommands']);
-Route::any('WebClientController', [WebClientController::class, 'processRequest']);
-
+Route::get('/', [PrintVanillaController::class, 'index']);
 Route::post('print', [PrintVanillaController::class, 'print'])->name('print');
-Route::get('vanilla', [PrintVanillaController::class, 'index']);
